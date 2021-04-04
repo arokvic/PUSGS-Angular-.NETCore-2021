7640013d11d9a157f081fdd0dcf08a8f2029d2e1
Sladja98 <sladjanavasiljevic4@gmail.com>
2021-04-04 11:53:00 +0200

    message1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import {FormGroup, FormControl, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'PUSGSprojekat';
+
 }
+
+
