d83307a48a5aa6c8ef19bdb467954f31816947bc
Sladja98 <sladjanavasiljevic4@gmail.com>
2021-04-08 23:10:48 +0200

    login, register components added, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { DashboardComponent} from './dashboard/dashboard.component'
+import { ContentComponent } from './content/content.component' 
+import { RegisterComponent} from './register/register.component'
+import { LoginComponent } from './login/login.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "dashboard",
+    component: DashboardComponent
+  },
+  {
+    path: "",
+    component: ContentComponent
+  },
+  {
+    path: "register",
+    component: RegisterComponent
+  }, 
+  {
+    path: "login",
+    component: LoginComponent
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ContentComponent } from './content/content.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ContentComponent,
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
