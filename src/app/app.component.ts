b5ee8739318460cdb2d42c4b2171055af40b093d
Sladja98 <sladjanavasiljevic4@gmail.com>
2021-04-16 01:47:24 +0200

    responsive navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators} from '@angular/forms'
+import { MenuItem } from './menu-item';
+import { Router, RouterLink }  from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +11,96 @@ import {FormGroup, FormControl, Validators} from '@angular/forms'
 export class AppComponent{
   title = 'PUSGSprojekat';
 
+  menuItems: MenuItem[] = [
+    {
+      label: 'Home',
+      icon: 'home',
+      showOnMobile: true,
+      showOnTablet: true,
+      showOnDesktop: true
+      
+    },
+    {
+      label: 'Register',
+      icon: 'assignment_ind',
+      showOnMobile: true,
+      showOnTablet: true,
+      showOnDesktop: true
+      
+    },
+    {
+      label: 'Log in',
+      icon: 'login',
+      showOnMobile: false,
+      showOnTablet: true,
+      showOnDesktop: true
+    },
+    {
+      label: 'Dashboard',
+      icon: 'dashboard',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Showcase',
+      icon: 'slideshow',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: true
+    },
+    {
+      label: 'Blog',
+      icon: 'rss_feed',
+      showOnMobile: false,
+      showOnTablet: false,
+      showOnDesktop: false
+    },
+  ];
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+  }
+
+  clickMenuItem(menuItem : MenuItem){
+    console.log(menuItem);
+    if(menuItem.label ==='Register')
+    {
+      this.router.navigate(['/register']);
+    }
+    if(menuItem.label ==='Log in')
+    {
+      this.router.navigate(['/login']);
+    }
+    if(menuItem.label ==='Home')
+    {
+      this.router.navigate(['']);
+    }
+    if(menuItem.label ==='Dashboard')
+    {
+      this.router.navigate(['/dashboard']);
+    }
+}
+clickMenuItem1(menuItem : MenuItem){
+  console.log(menuItem);
+  if(menuItem.label ==='Register')
+    {
+      this.router.navigate(['/register']);
+    }
+    if(menuItem.label ==='Log in')
+    {
+      this.router.navigate(['/login']);
+    }
+    if(menuItem.label ==='Home')
+    {
+      this.router.navigate(['']);
+    }
+  if(menuItem.label ==='Dashboard')
+  {
+    this.router.navigate(['/dashboard']);
+  }
 }
 
+}
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { ContentComponent } from './content/content.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButton, MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -18,7 +26,14 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
