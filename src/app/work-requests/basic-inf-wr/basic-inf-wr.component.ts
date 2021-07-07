58b77686caccba3ffa210a9ad58ad6fd4c19ff62
Sladja98 <sladjanavasiljevic4@gmail.com>
2021-07-07 02:28:30 +0200

    workRequest update

--- src/app/work-requests/basic-inf-wr/basic-inf-wr.component.ts
+++ src/app/work-requests/basic-inf-wr/basic-inf-wr.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { FormGroup, FormGroupDirective } from '@angular/forms';
+import { Router } from '@angular/router';
+//import { SwpInteractionService } from 'src/app/services/switching-plan/swp-interaction.service';
+import { WrInteractionService } from 'src/app/services/wr-interaction.service';
 
 @Component({
   selector: 'app-basic-inf-wr',
@@ -7,9 +11,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicInfWrComponent implements OnInit {
 
-  constructor() { }
+  
+  form!: FormGroup
+  startStatus: string = "Draft";
+  userCreatedBy: string = localStorage.getItem("username") || "";
+  
+  typeF:boolean = false;
+  streetF:boolean = false;
+  startDateF:boolean = false;
+  endDateF:boolean = false;
+  companyF:boolean = false;
+  phoneF:boolean = false;
+  badDate:boolean = false;
+
+
+  constructor(private rootFormGroup: FormGroupDirective, private router:Router, private wrService:WrInteractionService ) { }
 
   ngOnInit(): void {
+    this.form = this.rootFormGroup.control.get('basicInfo') as FormGroup
   }
 
+  
+
+  save(){
+    if(this.validate()){
+      console.log("tu sam, saljem poruku na navbar");
+      this.wrService.sendMessage(2);
+      this.router.navigate(['/work-requests/new/history-state']);
+    }
+    else{
+      
+    }
+
+  }
+
+  getError():string{
+    return "Error";
+  }
+
+  validate():boolean {
+    let retVal = true;
+    if(this.form.controls.type.hasError('required')){
+      this.typeF = true;
+      retVal = false;
+    }else{
+      this.typeF = false;
+    }
+    if(this.form.controls.street.hasError('required')){
+      this.streetF = true;
+      retVal = false;
+    }else{
+      this.streetF = false;
+    }
+    if(this.form.controls.startDate.hasError('required')){
+      this.startDateF = true;
+      retVal = false;
+    }else{
+      this.startDateF = false;
+    }
+    if(this.form.controls.endDate.hasError('required')){
+      this.endDateF = true;
+      retVal = false;
+    }else{
+      this.endDateF = false;
+    }
+    if(this.form.controls.company.hasError('required')){
+      this.companyF = true;
+      retVal = false;
+    }else{
+      this.companyF = false;
+    }
+    if(this.form.controls.phoneNo.hasError('required')){
+      this.phoneF = true;
+      retVal = false;
+    }else{
+      this.phoneF = false;
+    }
+    if(!this.startDateF && !this.endDateF){
+
+      let sDate = Date.parse(this.form.controls.startDate.value);
+      let eDate = Date.parse(this.form.controls.endDate.value);
+
+      console.log("Uporedjujem: " + sDate+ " i " +eDate);
+
+      if(eDate < sDate){
+        this.badDate = true;
+        retVal = false;
+      }else{
+        this.badDate = false;
+      }
+
+    }
+
+    return retVal;
+
+  }
+
+
 }
+
--- src/app/work-requests/navbar-wr/navbar-wr.component.ts
+++ src/app/work-requests/navbar-wr/navbar-wr.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+//import { SwpInteractionService } from 'src/app/services/switching-plan/swp-interaction.service';
+//import { SwitchingPlan } from 'src/app/entities/switchingPlan';
+//import { DocumentService } from 'src/app/services/document/document.service';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { WrInteractionService } from 'src/app/services/wr-interaction.service';
+import { WorkRequest } from 'src/app/entities/workRequest';
+import { DocumentWrService } from 'src/app/services/document-wr.service';
 
 @Component({
   selector: 'app-navbar-wr',
@@ -6,10 +14,105 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-wr.component.css']
 })
 export class NavbarWrComponent implements OnInit {
+  wrForm!: FormGroup;
+  activeId = 1;
+  documentId: number = -1;
+  workRequest!:WorkRequest;
 
-  constructor() { }
+  constructor(private http: HttpClient, private fb: FormBuilder, private wrService: WrInteractionService, private documentService: DocumentWrService) { }
 
   ngOnInit(): void {
+    this.wrService.currentActiveId$
+      .subscribe(
+        message => {
+          console.log('NavBarId: ' + message);
+          this.activeId = message;
+        }
+      );
+
+
+    this.wrForm = this.fb.group({
+      basicInfo: this.fb.group({
+        id: [],
+        type: ['', [Validators.required]],
+        workRequest: [],
+        status: 'Draft',
+        incident: [],
+        street: ['', [Validators.required]],
+        startDate: ['', [Validators.required]],
+        endDate: ['', [Validators.required]],
+        crew: [],
+        createdBy: localStorage.getItem('username'),
+        notes: [],
+        company: ['', [Validators.required]],
+        phoneNo: ['', [Validators.required]],
+        dateCreated: []
+      }),
+      historyState: this.fb.group({
+        dateChange: [],
+        changedBy: [],
+        newStatus: [],
+      }),
+      multimedia: this.fb.group({
+        imageData: []
+      }),
+      equipment: this.fb.group({
+        equipmentId: []
+      })
+    })
+    console.log('DocId: ' + this.wrService.getIdValue());
+
+    this.documentId = this.wrService.getIdValue();
+
+    if(this.documentId != null){
+      console.log('Popunjena forma')
+      this.http.get("https://localhost:44364/api/DocumentWr/GetWorkRequest?id="+this.documentId).subscribe(
+        data => {
+          this.workRequest = data as WorkRequest;
+
+          this.wrForm.setValue({
+            basicInfo: {
+              id: this.workRequest.id,
+              type: this.workRequest.type,
+              workRequest: this.workRequest.workRequest,
+              status: this.workRequest.status,
+              incident: this.workRequest.incident,
+              street: this.workRequest.street,
+              startDate: this.workRequest.startDate,
+              endDate: this.workRequest.endDate,
+              crew: this.workRequest.crew,
+              createdBy: this.workRequest.createdBy,
+              notes: this.workRequest.notes,
+              company: this.workRequest.company,
+              phoneNo: this.workRequest.phone,
+              dateCreated: this.workRequest.dateCreated
+            },
+            historyState: {
+              dateChange: [],
+              changedBy: [],
+              newStatus: [],
+            },
+            multimedia: {
+              imageData: this.workRequest.imageData
+            },
+            equipment: {
+              equipmentId: this.workRequest.equipment
+            }
+          })
+
+        }
+      )
+      console.log(this.workRequest);
+    }else{
+      console.log('Prazna forma');
+    }
+    
+
+  }
+
+  updateItem(newId:number){
+    console.log("pokusavam");
+    this.activeId = newId;
   }
 
 }
